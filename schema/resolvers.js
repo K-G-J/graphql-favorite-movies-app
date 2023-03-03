08257b3aa5c08dc902ab2fbf202492f8c5cbf2e9
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-03 17:10:06 -0500

    added movies queries

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,10 +1,25 @@
-const { UserList } = require('../FakeData')
+const { UserList, MovieList } = require('../FakeData')
+const _ = require('lodash')
 
 const resolvers = {
   Query: {
-    users() {
+    // USER RESOLVERS
+    users: (parent, args, context) => {
       return UserList
     },
+    user: (parent, { id }, context) => {
+      return _.find(UserList, { id: Number(id) })
+    },
+    // MOVIE RESOLVERS
+    movies: (parent, args, context) => {
+      return MovieList
+    },
+    movie: (parent, { name }, context) => {
+      return _.find(MovieList, { name })
+    },
+  },
+  User: {
+    favoriteMovies: (parent, args, context) => {},
   },
 }
 
